126a8e3f4c0a8268981a443211981962e5785f89
Rethelyi Nandor <n.rethelyi@gmail.com>
2026-01-22 23:40:10 +0100

    added signout button getter

--- POM/pages/ProfilePage.py
+++ POM/pages/ProfilePage.py
@@ -8,11 +8,16 @@ class ProfilePage(GeneralPage):
     def __init__(self, browser=None):
         super().__init__(browser, 'http://localhost:4200/user-profile')
         self.active_recipes_locator = (By.XPATH, '//span[text()=" Active recipes (0) "]')
+        self.button_sign_out_locator = (By.XPATH, '//p[@class="logout"]')
 
 
     def wait_for_pageload(self):
-        return WebDriverWait(self.browser, 5).until(EC.visibility_of_element_located(self.active_recipes_locator))
+        return WebDriverWait(self.browser, 5).until(EC.visibility_of_element_located(self.button_sign_out_locator))
 
 
     def get_active_recipes(self):
         return WebDriverWait(self.browser, 5).until(EC.visibility_of_element_located(self.active_recipes_locator))
+
+
+    def get_button_sign_out(self):
+        return WebDriverWait(self.browser, 5).until(EC.visibility_of_element_located(self.button_sign_out_locator))
