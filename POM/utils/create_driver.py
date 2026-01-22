ae6405ca72a67befcda9ca5b47ef3845d43ad51f
Rethelyi Nandor <n.rethelyi@gmail.com>
2026-01-22 23:18:02 +0100

    headless argument added

--- POM/utils/create_driver.py
+++ POM/utils/create_driver.py
@@ -6,7 +6,7 @@ def create_preconfigured_chrome_driver():
     options = Options()
     options.add_experimental_option("detach", True)
     options.add_argument('--guest')
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     browser = webdriver.Chrome(options=options)
     browser.set_window_size(1920, 1080)
     return browser
