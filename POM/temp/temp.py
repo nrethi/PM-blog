8c38ba3b485cd4e23a8c029e754876f682150d7b
Rethelyi Nandor <n.rethelyi@gmail.com>
2026-01-27 00:31:03 +0100

    fixed firefox webdriver options, experimented with pytest fixtures in temp

--- POM/temp/temp.py
+++ POM/temp/temp.py
@@ -1,1 +1,43 @@
-'change5'
+from selenium.webdriver.chrome.options import Options
+from requests import options
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.safari.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.webkitgtk.options import Options
+
+import pytest
+URL = 'https://duckduckgo.com'
+
+class MultiBrowserResponsiveness(object):
+
+    @pytest.fixture
+    def browser_chrome(self):
+        options = Options()
+        options.add_experimental_option("detach", True)
+        options.add_argument('--guest')
+        self.browser = webdriver.Chrome(options=options)
+        self.browser.maximize_window()
+        self.browser.get(URL)
+
+    @pytest.fixture
+    def browser_firefox(self):
+        options = Options()
+        options.add_experimental_option("detach", True)
+        options.add_argument('--guest')
+        self.browser = webdriver.Chrome(options=options)
+        self.browser.maximize_window()
+        self.browser.get(URL)
+
+    @pytest.fixture
+    def browser_safari(self):
+        options = Options()
+        options.add_experimental_option("detach", True)
+        options.add_argument('--guest')
+        self.browser = webdriver.Chrome(options=options)
+        self.browser.maximize_window()
+        self.browser.get(URL)
+
+def test_multiple_browsers(browser_chrome):
+    assert WebDriverWait()
--- POM/utils/create_driver.py
+++ POM/utils/create_driver.py
@@ -1,12 +1,11 @@
-from requests import options
+import selenium.webdriver
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.safari.options import Options
-from selenium.webdriver.webkitgtk.options import Options
+
 
 def create_preconfigured_chrome_driver():
-    options = Options()
+    options = selenium.webdriver.chrome.options.Options()
     options.add_experimental_option("detach", True)
     options.add_argument('--guest')
     options.add_argument('--headless')
@@ -14,17 +13,19 @@ def create_preconfigured_chrome_driver():
     browser.set_window_size(1920, 1080)
     return browser
 
+
 def create_preconfigured_firefox_driver():
-    options = Options()
-    options.add_experimental_option("detach", True)
+    options = selenium.webdriver.firefox.options.Options()
+    options.set_preference("detach", True)
     options.add_argument('--guest')
     options.add_argument('--headless')
     browser = webdriver.Firefox(options=options)
     browser.set_window_size(1920, 1080)
     return browser
 
+'''
 def create_preconfigured_safari_driver():
-    options = Options()
+    options = selenium.webdriver.safari.options.Options()
     options.add_experimental_option("detach", True)
     options.add_argument('--guest')
     options.add_argument('--headless')
@@ -40,6 +41,7 @@ def create_preconfigured_webkit_driver():
     browser = webdriver.WPEWebKit(options=options)
     browser.set_window_size(1920, 1080)
     return browser
+    '''
 
 
 
